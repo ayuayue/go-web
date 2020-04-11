593a320179f38e49e23ca0c8961d6cd01c34342a
ayuayue <1401262639@qq.com>
2020-04-11 21:35:46 +0800

    第三天,实现路由分组

--- gee/gee.go
+++ gee/gee.go
@@ -3,6 +3,7 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -11,22 +12,60 @@ type HandlerFunc func(*Context)
 
 //Engine 实现ServeHTTP接口
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
+}
+
+//RouterGroup 路由组
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
 }
 
 //New 实例化一个Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+//Group 路由组
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+//addRoute 路由组添加路由
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Router %4s = %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 //addRoute 添加路由
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
 	engine.router.addRoute(method, pattern, handler)
 }
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
 
 //GET 方法
 func (engine *Engine) GET(pattern string, handler HandlerFunc) {
 	engine.addRoute("GET", pattern, handler)
+}
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 //POST 方法
